9ea2b61204fa2ccdaaa7ca9898c25400d6f2206e
VodenoFF <freshboybg@gmail.com>
2021-03-04 23:24:31 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+game { 'gta5' }
+
+fx_version 'adamant'
+author 'VodenoFF'
+description 'Skeletal system'
+
+lua54 'yes'
+
+client_scripts {
+	 'client.lua'
+}
+
+server_scripts {	
+	'server.lua',
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/index.js',
+	'html/index.css'
+}
